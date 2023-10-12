bafdb15e7d6a0f106c7ff442e2b78c2b2e019c42
XPanel <127631174+xpanel-cp@users.noreply.github.com>
2023-10-12 23:19:11 +0330

    Update dropbear.sh

--- dropbear.sh
+++ dropbear.sh
@@ -3,46 +3,28 @@
 
 json_output="["
 
-port_dropbear=$(ps aux | grep dropbear | awk NR==1 | awk '{print $17;}')
 log="/var/log/auth.log"
 loginsukses="Password auth succeeded"
 
-pids=$(ps ax | grep dropbear | grep " $port_dropbear" | awk -F" " '{print $1}')
+time_5_hours_ago=$(date -d "5 hours ago" "+%b %d %T")
 
-for pid in $pids
-do
-    pidlogs=$(grep $pid $log | grep "$loginsukses" | awk -F" " '{print $3}')
-    i=0
-    for pidend in $pidlogs
-    do
-      let i=i+1
-    done
-    if [ $pidend ];then
-       login=$(grep $pid $log | grep "$pidend" | grep "$loginsukses")
-       PID=$pid
-       user=$(echo $login | awk -F" " '{print $10}' | sed -r "s/'/ /g")
-       user=$(echo $user | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
-       waktu=$(echo $login | awk -F" " '{print $2"-"$1,$3}')
-       while [ ${#waktu} -lt 13 ]; do
-           waktu=$waktu" "
-       done
-       while [ ${#user} -lt 16 ]; do
-           user=$user" "
-       done
-       while [ ${#PID} -lt 8 ]; do
-           PID=$PID" "
-       done
-       PID=$(echo $PID | sed -r -e "s/'/ /g" -e "s/ *$//" -e "s/ //g")
-       user=$(echo $user | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
-       json_item="{"
-       json_item+="\"user\": \"$user\", "
-       json_item+="\"PID\": \"$PID\", "
-       json_item+="\"waktu\": \"$waktu\""
-       json_item+="}"
-       json_output+=" $json_item,"
+while IFS= read -r line; do
+    if [[ "$line" == *"$loginsukses"* && "$line" > *"$time_5_hours_ago"* ]]; then
+        pid=$(echo "$line" | awk '{print $5}' | sed "s/'//g")
+        pid=$(echo "$pid" | grep -oP '\[\K\d+(?=\]:)')
+        waktu=$(echo "$line" | awk '{print $1 "-" $2}')
+        user=$(echo "$line" | awk '{print $10}' | sed "s/'//g")
+        user=$(echo -e "$user" | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
 
+        json_item="{"
+        json_item+="\"user\": \"$user\", "
+        json_item+="\"PID\": \"$pid\", "
+        json_item+="\"waktu\": \"$waktu\""
+        json_item+="}"
+        json_output+=" $json_item,"
     fi
-done
+done < "$log"
+
 json_output="${json_output%,}"
 json_output+="]"
 echo "$json_output" > /var/www/html/app/storage/dropbear.json
